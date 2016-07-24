d73422eaff62c17ca0986eeac5fd12c039d324d6
Ben Weinstein-Raun <b@w-r.me>
2016-07-24 11:12:05 -0700

    add docstrings

--- puz.py
+++ puz.py
@@ -2,10 +2,6 @@ DEC = "0123456789"
 CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ALPHA = 'abcdefghijklmnopqrstuvwxyz'
 BASE64 = CAPS + ALPHA + DEC + '+/'
-FUCKED64 = DEC + CAPS + ALPHA + '+/'
-FUCKED65 = ALPHA + CAPS + DEC + '+/'
-FUCKED66 = '+/' + CAPS + ALPHA + DEC
-FUCKED67 = DEC + ALPHA + CAPS + '+/'
 HEX = DEC + ALPHA[:6]
 ASCII = (' ' * 33 + '!"#$%&\'()*+,-./' + DEC + ':;<=>?@' +
          CAPS + "[\\]^_`" + ALPHA + "{|}~ ")
@@ -15,6 +11,20 @@ EN_CHAR_FREQS = [82, 15, 28, 43, 127, 22, 20, 61, 70, 2, 8, 40, 24,
 
 
 def from_base(s, base=16, alphabet=HEX):
+    """Interpret `s` as representing a value in the specified base/alphabet.
+
+    (basically a slightly fancier version of `int(s, base)`)
+
+    s: The string to read. Must contain only characters in `alphabet`.
+    alphabet: The character set to assume `s` is using. There can be more
+         than `base` characters in `alphabet`; only the first `base` characters
+         are used. Defaults to the HEX character ordering ('01234567890abcdef').
+         Other alphabets are available: ASCII, BASE64, ALPHA, CAPS
+    base: The base in which the string should be interpreted. Defaults
+         to 16 for hex interpretation.
+
+    Returns a python integer.
+    """
     indices = {}
     for i, a in enumerate(alphabet):
         indices[a] = i
@@ -27,6 +37,20 @@ def from_base(s, base=16, alphabet=HEX):
 
 
 def to_base(i, base=16, alphabet=HEX, minwidth=1):
+    """Stringify an integer using digits from a given alphabet.
+
+    i: The integer to interpret.
+    base: The base to use. Must be no larger than the `len(alphabet)`.
+    alphabet: A string of digits to use to encode the alphabet. Defaults to
+         the hexadecimal characters ('0123456789abcdef').
+    minwidth: If the output would be shorter than this width,
+         it will be padded with the zero character until it is this long.
+         Defaults to 1, which will cause an input of `0` to be shown as
+         the zero character. If it were 0, `0` would be shown as the empty
+         string.
+
+    Returns a python string.
+    """
     result = []
     neg = i < 0
     if neg:
@@ -43,11 +67,27 @@ def to_base(i, base=16, alphabet=HEX, minwidth=1):
 
 
 def convert_base(s, base_a, alpha_a, base_b, alpha_b):
+    """Do from_base, and then do to_base with the result."""
     val = from_base(s, base=base_a, alphabet=alpha_a)
     return to_base(val, base=base_b, alphabet=alpha_b)
 
 
 def rot_n(s, shift=13, base=26, alphabet=ALPHA, negate=False):
+    """Caesar cipher.
+    
+    s: The string to shift.
+    shift: The (integer) direction to shift through the alphabet.
+         Defaults to 13.
+    base: The base to shift through
+    alphabet: The characterset to shift through. If a character
+         is not in this alphabet, it will be left alone in the
+         result. Defaults to *ONLY LOWERCASE* characters. A
+         more principled way to handle this would be to accept
+         a list of alphabets as the argument (to allow lower
+         and upper case to be shifted separately, or for
+         other weird ciphers)
+    negate: I needed this once but probably never will again.
+    """
     indices = {}
     for i, a in enumerate(alphabet):
         indices[a] = i
